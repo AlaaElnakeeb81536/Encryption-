0fdebae3684de931253927b1e604a34ad1a75d37
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2022-12-25 19:20:06 +0200

    Add files via upload

---
+++ index3.py
@@ -0,0 +1,14 @@
+#try to find num of word and num of charachter 
+def count (sentence):
+    res=0
+    c=0
+    for word in sentence.split():
+        res+=1
+        for i in word:
+            c+=1
+    print(res) #res refer to num of word in sentence
+    print(c) #c refers to num of char in sentence 
+count("Alaa Elnakeeb")
+            
+        
+        
